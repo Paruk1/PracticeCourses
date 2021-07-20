998f1f9660c0d1d0b6ca500a8115cdb0b0a6d8d8
Paruk1 <parail200111@gmail.com>
2021-07-20 13:40:35 +0300

    Practice 1-4

--- src/com/alfabank/Task21.java
+++ src/com/alfabank/Task21.java
@@ -6,7 +6,5 @@ public class Task21 {
     public static void main(String[] args) {
         Runner runner = new Runner();
         runner.run();
-
-
     }
 }
--- src/com/alfabank/work11/Runner.java
+++ src/com/alfabank/work11/Runner.java
@@ -2,10 +2,30 @@ package com.alfabank.work11;
 
 public class Runner {
     public void run(){
-        Book book1 = new Book();
-        Book book2 = new Book(1,"Parail","Privetos","Kiev",2001,356,150.50);
+        outInfo(addInterestToBook(createMassive(),10));
+    }
 
-        book1.view();
-        book2.view();
+    public Book[] addInterestToBook(Book[] array, double interest){
+        for (Book b : array) {
+            b.setCost((b.getCost() * interest / 100) + b.getCost());
+        }
+        return array;
+    }
+
+    private Book[] createMassive(){
+        return new Book[]{
+                new Book(1, "Parail", "Privetos", "Kiev", 1, 356, 150.50),
+                new Book(2, "Parail1", "Privetos1", "Kiev1", 100, 123, 100),
+                new Book(3, "Parail2", "Privetos2", "Kiev2", 1000, 3434, 22222),
+                new Book(4, "Parail3", "Privetos3", "Kiev3", 1500, 33456, 232.2),
+                new Book(5, "Parail4", "Privetos4", "Kiev4", 2000, 3356, 150.50),
+                new Book(6, "Parail5", "Privetos5", "Kiev6", 2001, 35446, 765.52),
+                new Book(7, "Parail6", "Privetos6", "Kiev7", 3000, 35336, 150.50)
+        };
+    }
+    private void outInfo(Book[] array){
+        for (Book b : array) {
+            b.view();
+        }
     }
 }
