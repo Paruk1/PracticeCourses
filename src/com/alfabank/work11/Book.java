ee26cba114a68f9238f82e87b3fa06a7a9cf1367
Paruk1 <parail200111@gmail.com>
2021-07-22 11:31:01 +0300

    Practice 1-5 - updated

--- src/com/alfabank/work11/Book.java
+++ src/com/alfabank/work11/Book.java
@@ -10,7 +10,7 @@ public class Book {
     private double cost;
 
 
-    public Book(){
+    public Book() {
         id = 0;
         author = "";
         title = "";
@@ -33,53 +33,62 @@ public class Book {
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public String getAuthor() {
         return author;
     }
+
     public void setAuthor(String author) {
         this.author = author;
     }
+
     public String getTitle() {
         return title;
     }
+
     public void setTitle(String title) {
         this.title = title;
     }
+
     public String getPublish() {
         return publish;
     }
+
     public void setPublish(String publish) {
         this.publish = publish;
     }
+
     public int getYear() {
         return year;
     }
+
     public void setYear(int year) {
         this.year = year;
     }
+
     public int getPage() {
         return page;
     }
+
     public void setPage(int page) {
         this.page = page;
     }
+
     public double getCost() {
         return cost;
     }
+
     public void setCost(double cost) {
         this.cost = cost;
     }
 
     public void view() {
-        System.out.println(getId() + " "
-                + getPage() + " "
-                + getYear() + " "
-                + getTitle() + " "
-                + getCost() + " "
-                + getAuthor() + " "
-                + getPublish());
+        System.out.printf(" " + this.id + " " + this.author+ " " + this.title + " " + this.publish + " " +
+                this.year + " " + this.page + " " + this.cost + "\n", "%d%s%n%s%n%s%n%d%d%f");
+
     }
 }
--- src/com/alfabank/work11/Runner.java
+++ src/com/alfabank/work11/Runner.java
@@ -1,60 +1,90 @@
 package com.alfabank.work11;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Runner {
-    public void run(){
-        Scanner author = new Scanner(System.in);
-        Scanner publish = new Scanner(System.in);
-        Scanner year = new Scanner(System.in);
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        Book[] createdMassive = createMassive();
+        outInfo(createdMassive);
 
-        System.out.println("Введите имя автора: ");
-        findAuthor(createMassive(),author.nextLine());
-        System.out.println("Введите издательство: ");
-        findPublish(createMassive(),publish.nextLine());
-        System.out.println("Введите год: ");
-        findYear(createMassive(),year.nextInt());
+        System.out.println("\nВведите имя автора: ");
+
+        Book[] authors = findAuthor(createdMassive, scanner.nextLine());
+        if (authors.length == 0) {
+            System.out.println("Автора с этим именем не найдено");
+        } else{
+            outInfo(authors);
+        }
+
+        System.out.println("\nВведите название издательства: ");
+
+        Book[] publishes = findPublish(createdMassive, scanner.nextLine());
+        if (publishes.length == 0) {
+            System.out.println("Издательство не найдено");
+        } else{
+            outInfo(publishes);
+        }
+
+        System.out.println("\nВведите год: ");
+
+        Book[] years = findYear(createdMassive, scanner.nextInt());
+        if (years.length == 0) {
+            System.out.println("Нет выпуска после указанного года");
+        } else{
+            outInfo(years);
+        }
+
+
     }
 
-    private void findAuthor(Book[] array,String name){
+    private Book[] findAuthor(Book[] array, String name) {
         int count = 0;
+        Book[] temp = new Book[array.length];
         for (Book b : array) {
-            if(b.getAuthor().equals(name)){
-                b.view();
+            if (b.getAuthor().equalsIgnoreCase(name)) {
+                temp[count] = b;
                 count++;
             }
         }
-        if(count == 0) System.out.println("Автора с этим именем не найдено");
+        return Arrays.copyOf(temp, count);
     }
-    private void findPublish(Book[] array,String name){
+
+    private Book[] findPublish(Book[] array, String name) {
         int count = 0;
+        Book[] temp = new Book[array.length];
         for (Book b : array) {
-            if(b.getPublish().equals(name)){
-                b.view();
+            if (b.getPublish().equalsIgnoreCase(name)) {
+                temp[count] = b;
                 count++;
             }
         }
-        if(count == 0) System.out.println("Издевательство не найдено");
+        return Arrays.copyOf(temp,count);
     }
-    private void findYear(Book[] array,int year){
+
+    private Book[] findYear(Book[] array, int year) {
         int count = 0;
+        Book[] temp = new Book[array.length];
+
         for (Book b : array) {
-            if(b.getYear() > year){
-                b.view();
+            if (b.getYear() > year) {
+                temp[count] = b;
                 count++;
             }
         }
-        if(count == 0) System.out.println("Нет выпуска после указанного года");
+        if (count == 0) System.out.println("Нет выпуска после указанного года");
+        return Arrays.copyOf(temp,count);
     }
 
 
-    private Book[] addInterestToBook(Book[] array, double interest){
+    private void addInterestToBook(Book[] array, double interest) {
         for (Book b : array) {
             b.setCost((b.getCost() * interest / 100) + b.getCost());
         }
-        return array;
     }
-    private Book[] createMassive(){
+
+    private Book[] createMassive() {
         return new Book[]{
                 new Book(1, "Parail", "Privetos", "Kiev", 1, 356, 150.50),
                 new Book(2, "Parail1", "Privetos1", "Kiev1", 100, 123, 100),
@@ -65,7 +95,8 @@ public class Runner {
                 new Book(7, "Parail6", "Privetos6", "Kiev7", 3000, 35336, 150.50)
         };
     }
-    private void outInfo(Book[] array){
+
+    private void outInfo(Book[] array) {
         for (Book b : array) {
             b.view();
         }
